bf243471c101f704367b057234085fcf386801ba
Maneet Lodha <maneetlodha@rocketmail.com>
2020-11-28 19:16:00 +0000

    final

--- index.js
+++ index.js
@@ -17,47 +17,47 @@ var quesList = [{
   question: chalk.hex('#BDBDBD')("In Web series FRIENDS, who amoung them is vegetarian?\n1. Ross\n2. Joey\n3. Chandler\n4. Phoebe\n"),
   answer: 4
 }
-,{
+  , {
   question: chalk.hex('#BDBDBD')("Which Latino artist/s saw their music video become the first video in YouTube history to reach 4bn views??\n1. Shakira\n2. Camila Cabello Brin\n3. Luis Fonsi and Daddy Yankee\n4. Eminem\n"),
   answer: 3
-},{
-  question:chalk.hex('#BDBDBD')("Name of the Space AirCraft Company Founded by Elon Musk?\n1. Galaxy X\n2. Star X\n3. Space X\n4. None\n"),
+}, {
+  question: chalk.hex('#BDBDBD')("Name of the Space AirCraft Company Founded by Elon Musk?\n1. Galaxy X\n2. Star X\n3. Space X\n4. None\n"),
   answer: 3
 }];
 
 //DRIVER PROGRAM
 
 var userName = readlineSync.question("Enter your Name: ");
-console.log(chalk.red("Welcome " +userName+ " to the Pop Culture quiz\n"));
+console.log(chalk.red("Welcome " + userName + " to the Pop Culture quiz\n"));
 console.log();
-console.log(chalk.bgRed("Instructions:")+chalk.black.bgWhite.bold("\n 1. Enter option number to select the answer.\n"));
+console.log(chalk.bgRed("Instructions:") + chalk.black.bgWhite.bold("\n 1. Enter option number to select the answer.\n"));
 
 var score = [];
-function quizGame(quesObject){
+function quizGame(quesObject) {
   var gameScore = 0;
-  
-  for(i=0;i<quesObject.length;i++){
+
+  for (i = 0; i < quesObject.length; i++) {
     var userAns = readlineSync.question(quesObject[i].question);
-    if(userAns == quesObject[i].answer){
-      gameScore = gameScore+1;
+    if (userAns == quesObject[i].answer) {
+      gameScore = gameScore + 1;
       console.log(chalk.bold.hex('#3485e4')("Correct Answer!!"));
-    }else{
+    } else {
       console.log(chalk.bold.red("Sad! wrong answer, but you can surely try again"));
     }
     console.log("------------------------------");
   }
   score.push(gameScore);
-  console.log(chalk.hex('#009688')("Thanks for playing: ",chalk.white.bgCyan.bold(userName)));
-  console.log(chalk.hex('#009688')("your score is: "+gameScore+"/"+quesList.length));
+  console.log(chalk.hex('#009688')("Thanks for playing: ", chalk.white.bgCyan.bold(userName)));
+  console.log(chalk.hex('#009688')("your score is: " + gameScore + "/" + quesList.length));
   var max = score.reduce(function(a, b) {
     return Math.max(a, b);
   });
-  console.log(chalk.green("your highest score is: "+ max + "🎊🎉"));
+  console.log(chalk.green("your highest score is: " + max + "🎊🎉"));
 
   var playAgain = readlineSync.question(chalk.black.bgWhite.bold("Want to play again?  [y/n] \n"));
-  if(playAgain == "y"){
+  if (playAgain == "y") {
     quizGame(quesObject);
-  }else{
+  } else {
     console.log("see you soon, Thanks for playing 🔰 ");
   }
 }
